8cf76e06c1a0c72e12af3e40d21770efafb9ea27
royelhames <60118104+royelhames@users.noreply.github.com>
2020-01-22 13:01:47 +0000

    Add files via upload

--- lib/log_file.rb
+++ lib/log_file.rb
@@ -1,1 +1,34 @@
+require 'mimemagic'
+class LogFile
+  attr_reader :path
+  attr_reader :status
+  attr_reader :file  
+
+  def initialize(path)
+    @path = path
+    @status = status
+    @file = file
+  end 
+
+  def status
+    status = true
+    unless eval(File.exists?(@path.to_s).to_s)
+      status = nil
+      raise LogFileError,"File does n't exit or not a regular file"
+    end
+  
+    if File.size(@path.to_s).to_i == 0
+      status = nil
+      raise LogFileError,"Empty file"
+    end
+  end
+
+  def file
+     file = File.new(@path)
+  end
+end
+
+class LogFileError < StandardError
+   
+end
 
